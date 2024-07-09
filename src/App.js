5f0f391464dff53a7a636cdec4c034e556233e1f
kalvin104 <jpwickham@hotmail.co.uk>
2024-07-09 12:58:44 +0100

    removed socialicon react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './layout/Footer';
 import Homepage from './pages/Homepage';
 import Header from './layout/Header';
 
+import React from 'react'
+
 function App() {
   return (
     <div className="App">
